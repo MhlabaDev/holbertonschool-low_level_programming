3568ed17a5bf8854c54ac1c06bf35810dc5ddb60
Mhlaba Mkhatshane <hlabisto28@gmail.com>
2025-06-10 11:22:47 +0000

    Question 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+    for (n = 0; n < 10; n++)
+        putchar(n + '0');
+    for (n = 10; n < 16; n++)
+        putchar(n - 10 + 'a');
+    putchar('\n');
+    return (0);
+}
