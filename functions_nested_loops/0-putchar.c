b97040d7994109e4e012b5cb86e2b284370ff406
MhlabaDev <hlabisto28@gmail.com>
2025-06-10 14:06:32 +0200

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
+#include "main.h"
 
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('' );
-    _putchar('\n');
-    return (0);
+	char c[] = "_putchar\n";
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	return (0);
 }
