666e16617d80920dd0e76ee02d9fceddc6f221f8
MhlabaDev <hlabisto28@gmail.com>
2025-06-10 16:14:27 +0200

    Update 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * print_square - prints a square
- * @size: size of the square
+ * print_square - prints a square, followed by a new line;
+ * @n: nunber to be squared
  */
-void print_square(int size)
+void print_square(int n)
 {
-    if (size <= 0)
-        _putchar("\\n");
-    else
-    {
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-                _putchar("#");
-            _putchar("\\n");
-        }
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
