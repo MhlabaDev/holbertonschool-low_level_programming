dc78dc88f181eabb0f5016cf0440984b95962843
MhlabaDev <hlabisto28@gmail.com>
2025-06-10 13:29:17 +0200

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - reverse printing then new line
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-    char c;
-    for (c = 'z'; c >= 'a'; c--)
-        putchar(c);
-    putchar('\n');
-    return (0);
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
