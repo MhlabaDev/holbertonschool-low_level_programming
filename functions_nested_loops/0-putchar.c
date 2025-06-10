d2f3dcbe9badd62baebaf33f6e172caa15812e0d
Mhlaba Mkhatshane <hlabisto28@gmail.com>
2025-06-10 12:11:58 +0000

    update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,7 @@
+#include "main.h"
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('' );
-    _putchar('\n');
+    print_alphabet();
     return (0);
 }
