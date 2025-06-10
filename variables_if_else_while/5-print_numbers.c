0a58e70a2fd258a867dec4c7604531c601f99df3
MhlabaDev <hlabisto28@gmail.com>
2025-06-10 13:23:53 +0200

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,17 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Description: prints single digit numbers of base 10
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    int n;
-    for (n = 0; n < 10; n++)
-        putchar(n + '0');
-    putchar('\n');
-    return (0);
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
 }
