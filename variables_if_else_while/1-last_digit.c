92aed1880d8e108fa6f0a2a200ec264126141552
MhlabaDev <hlabisto28@gmail.com>
2025-06-10 12:19:21 +0200

    Update 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,17 +4,18 @@
 
 /**
  * main - Entry point
- * Description: checks on the last digit of the assigned var
- * Return: Always 0 (success)
+ * Description: Checks the last digit of the assigned variable.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n, a;
+	int n;
+	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
+
 	if (a > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
