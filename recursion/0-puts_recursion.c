9c281559e22e48fcd04575cf4ee5dbd6a2d9163d
MhlabaDev <hlabisto28@gmail.com>
2025-06-11 10:11:22 +0200

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - Prints a string followed by a new line
- * @s: input
- * Return: Always 0 (Success)
+ * @s: The string to print
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s)  /* Base case: check if the string is not empty */
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(*s);  /* Print the current character */
+		_puts_recursion(s + 1);  /* Recursive call with the next character */
 	}
-
 	else
-		_putchar(n);
+	{
+		_putchar('\n');  /* Print a new line at the end */
+	}
 }
