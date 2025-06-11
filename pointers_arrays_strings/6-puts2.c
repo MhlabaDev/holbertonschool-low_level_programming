5a4f7dd5b6fe667eb2612d4c5588eea8e168fae9
MhlabaDev <hlabisto28@gmail.com>
2025-06-11 08:49:10 +0200

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,22 @@
-#include "main.h"
-/**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
-void puts2(char *str)
-{
-	int i = 0;
-	int t = 0;
-	char *y = str;
-	int a;
+#include	"main.h"
 
-	while (*y != 0)
-	{
-		y++;
-		i++;
-	}
-	t = i - 1;
-	for (a = 0 ; a <= t ; a++)
-	{
-		if (a % 2 == 0)
-	{
-		_putchar(str[a]);
-	}
-	}
-	_putchar(n);
+/**
+*	puts2	-	Prints	every	other	character	of	a	string
+*	@str:	The	string	to	process
+*
+*	Return:	void
+*/
+void	puts2(char	*str)
+{
+int	i	=	0;
+
+while	(str[i]	!=	'\0')
+{
+if	(i	%	2	==	0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
 }
